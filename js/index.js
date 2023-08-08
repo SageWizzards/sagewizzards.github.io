612f8e88b34f227e9b8d5f393e70c7becf2b793a
Nel Alejandro Mora Pinto <nelmora.09@gmail.com>
2023-08-08 15:48:02 -0500

    Initial settings and chat section

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+// Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+    
+    
+
+    // Obtiene el botón por su id
+    var boton = document.getElementById("getStarted");
+
+    // Agrega un evento de clic al botón
+    boton.addEventListener("click", function() {
+        // Coloca aquí el código que deseas ejecutar al presionar el botón
+        console.log("¡Hiciste clic en el botón!");
+        
+    });
+});
