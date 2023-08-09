939a16bdfd1c831a6f00f785a730bf94a46b8173
Nel Alejandro Mora Pinto <nelmora.09@gmail.com>
2023-08-09 14:53:14 -0500

    Sage chat styles

--- js/index.js
+++ js/index.js
@@ -2,10 +2,21 @@
 // Espera a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function() {
     
-    
+    const GET_STARTED = 'getStarted';
+    const GET_STARTED_BUTTON = 'getStartedButton';
+    let getStarted = localStorage.getItem(GET_STARTED);
 
+    // Initial 
+    if(getStarted === null) {
+        localStorage.setItem(GET_STARTED, false);
+    }
+
+    if(getStarted === false) {
+        
+    }
+    
     // Obtiene el botón por su id
-    var boton = document.getElementById("getStarted");
+    let boton = document.getElementById(GET_STARTED_BUTTON);
 
     // Agrega un evento de clic al botón
     boton.addEventListener("click", function() {
