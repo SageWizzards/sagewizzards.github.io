f447992d81697c15f573e37ac313608d039fb367
Camila Morales <camilamo9719@gmail.com>
2023-08-29 01:34:41 -0500

    movements for carousel finished

--- js/carousel.js
+++ js/carousel.js
@@ -5,7 +5,7 @@ const arrowBtns = document.querySelectorAll(".team-wrapper i");
 const firstCardWidth = carousel.querySelector(".team-card").offsetWidth;
 const carouselChildrens = [...carousel.children];
 
-let isDragging = false, startX, startScrollLeftm, timeoutId;
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
 //Obtiene el numero de cards que pueden encajar en el carrusel
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
@@ -19,16 +19,18 @@ carouselChildrens.slice(0, cardPerView).forEach(card => {
     carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
 //Para que funcionen los botones del carrusel
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        //Mostrar id del boton en consola
-        console.log(btn.id);
-        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
-    })
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
 });
 
-const dragStart = () => {
+const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
     //Recuerda la posicion inicial del cursor y scrollea el carrusel
@@ -50,32 +52,30 @@ const dragStop = () => {
     carousel.classList.remove("dragging");
 }
 
-//Se añade autoplay
-//Retornará si la ventana es menor a 800px
-const autoPlay = () => {
-    if(window.innerWidth < 800) return;
-    //Autoplay el carrusel cada 2500 milisegundos
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-}
-
-autoPlay();
-
-//Scroll infinito
 const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
     if(carousel.scrollLeft === 0) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
         carousel.classList.remove("no-transition");
-    } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        //console.log("Fin del lado derecho");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("no-transition");
     }
-
+    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
     clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay;
+    if(!wrapper.matches(":hover")) autoPlay();
 }
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+    // Autoplay the carousel after every 2500 ms
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
 
 //Draggins
 carousel.addEventListener("mousedown", dragStart);
